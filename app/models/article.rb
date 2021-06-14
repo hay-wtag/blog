c5e730485c4bc29e24380babea33cf1d7493a6dc
WellDev <syed.hassan@welldev.io>
2021-06-14 13:42:14 +0600

    added validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 30 }
-  validates :description, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 1000 }
 end
