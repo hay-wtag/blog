bba576a7de42088b4c953d223361d145d7d9ac07
WellDev <syed.hassan@welldev.io>
2021-06-14 15:39:21 +0600

    added user data in ui

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 3, maximum: 30 }
+  belongs_to :user
+  validates :title, presence: true, length: { minimum: 2, maximum: 30 }
   validates :description, presence: true, length: { minimum: 5, maximum: 1000 }
+  validates :user_id, presence: true
 end
